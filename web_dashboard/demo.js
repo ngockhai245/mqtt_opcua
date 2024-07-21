897f17f92320cfa9f24602b1b51a9991851b3239
ngockhai245 <phamngockhai245@gmail.com>
2024-07-21 23:15:17 +0700

    bug in demo.js, not fixed :((

--- web_dashboard/demo.js
+++ web_dashboard/demo.js
@@ -1,31 +1,28 @@
-function onConnect(){
-    clientID = "clientID - " + parseInt(Math.random() * 100);
+function startConnect(){
+    clientID = "clientID - " + parseInt(Math.random() * 1000);
+    
     host = document.getElementById("host").value;
     port = document.getElementById("port").value;
     userID = document.getElementById("username").value;
     passwordID = document.getElementById("password").value;
 
-    document.getElementById("messages").innerHTML += "<span>Connecting to " + host + "on port " + port + "</span><br>";
+    document.getElementById("messages").innerHTML += "<span>Connecting to " + host + " on port " + port + "</span><br>";
     document.getElementById("messages").innerHTML += "<span>Using the client ID " + clientID + "</span><br>";
 
-    client = new paho.MQTT.Client(host, Number(port), clientID);
+    client = new Paho.MQTT.Client(host, Number(port), clientID);
 
     //su dung bien client de su dung cac chuc nang khac cua thu vien
     client.onConnectionLost = onConnectionLost;
     client.onMessageArrived = onMessageArrived;
 
-    client.connect({
-        onSuccess: onConnect,
-        userName: userID,
-        passwordID: passwordID
-    })
+    client.connect({ onSuccess: onConnect });
 }
 
 function onConnect() {
     topic = document.getElementById("topic_sub").value;
-    document.getElementById("message").innerHTML += "<span>Subscribing to topic " + topic + "</span><br>"
+    document.getElementById("messages").innerHTML += "<span>Subscribing to topic " + topic + "</span><br>";
     
-    client.subscribe();
+    client.subscribe(topic);
 }
 
 function onConnectionLost(responseObject) {
@@ -40,10 +37,19 @@ function onMessageArrived(message) {
     document.getElementById("messages").innerHTML += "<span>Topic: " + message.destinationName + "| Message : " + message.payloadString + "<span><br>";
 }
 
-function onDisconnect(){
-
+function startDisconnect(){
+    client.disconnect();
+    document.getElementById("messages").innerHTML += "<span>Disconnected. </span><br>";
 }
 
 function publishMessage(){
+    msg = document.getElementById("message").value;
+    topic = document.getElementById("topic_pub").value;
 
+    Message = new Paho.MQTT.Message(msg);
+    Message.destinationName = topic;
+
+    client.send(Message);
+    
+    document.getElementById("messages").innerHTML += "<span> Message to topic " + topic + " is sent </span><br>";
 }
